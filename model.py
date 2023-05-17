bug fix: remove softmax

model.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 import numpy as np
-#TODO
-# 1. generate
+
 
 class GELU(nn.Module):
     def __init__(self):
@@ -91,7 +90,6 @@ class Transformer(nn.Module):
             x = l(x)
         x = self.layer_norm(x)
         x = self.softmax_proj(x) # [B, C, vocab]
-        x = F.softmax(x, dim=-1)
         return x
 
     def get_optimizer(self):

train.py
@@ -45,7 +45,7 @@ def train(dataloader, model, loss_fn, optimizer):
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
         pred = model(X)
-        loss = loss_fn(pred.view(-1, enc.n_vocab), y.view(-1))
+        loss = loss_fn(pred.view(-1, pred.size(-1)), y.view(-1))
         optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
@@ -64,7 +64,7 @@ def validate(dataloader, model, loss_fn):
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
-            test_loss += loss_fn(pred.view(-1, enc.n_vocab), y.view(-1)).item()
+            test_loss += loss_fn(pred.view(-1, pred.size(-1)), y.view(-1)).item()
     test_loss /= num_batches
     print(f"Avg loss: {test_loss:>8f} \n")
 